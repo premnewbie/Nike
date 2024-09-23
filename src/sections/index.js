f9009efec2d30e245450e20a1d4644baceae4321
premnewbie <prempm50@gmail.com>
2024-09-23 22:29:17 +0530

    completed till Hero section

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+
+
+ export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    Footer,
+    SuperQuality,
+    CustomerReviews,
+    Services,
+    SpecialOffer
+ }
