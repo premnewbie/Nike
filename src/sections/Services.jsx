a57af156e6a9c931f7458ac5a8435ed1c94c3995
premnewbie <prempm50@gmail.com>
2024-09-24 15:29:19 +0530

    Completed built

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,11 @@
+import ServiceCard from "../Components/ServiceCard"
+import { services } from "../Constants"
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+        {services.map((service) => (<ServiceCard key={service.label} {...service} />))}
+    </section>
   )
 }
 
